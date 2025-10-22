30de08fa3100fd6fe434003bfc810133d8722af7
Ctrl-Ze <alexandrucretug@gmail.com>
2025-10-22 13:43:35 +0300

    init commit

---
+++ src/native-test/java/com/healthatlas/auth/AuthResourceIT.java
@@ -0,0 +1,8 @@
+package com.healthatlas.auth;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class AuthResourceIT extends AuthResourceTest {
+    // Execute the same tests but in packaged mode.
+}
